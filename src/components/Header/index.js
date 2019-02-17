b51dc133017e9343e092ed0d9e131397fe6133f6
ajitesh-30 <ajiteshs10@gmail.com>
2019-02-17 23:25:51 +0530

    Adding all files

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,15 +5,14 @@ class Header extends React.Component {
 	render(){
 		return (	
 			<nav class="nav-class">
-				<span class="nav-text">#Zypher</span>
 				<span class="glyphicon glyphicon-name"></span>
 				<div class="nav-icons">
 					<span class="icons"><i class="fa fa-search fa-lg" aria-hidden="true"></i></span>
 					<span class="icons"><i class="fa fa-bell fa-lg" aria-hidden="true"></i></span>
 					<span class="icons"><i class="fa fa-bookmark-o fa-lg" aria-hidden="true"></i></span>
 					<span class="icons"><i class="fa fa-shopping-bag fa-lg" aria-hidden="true"></i></span>
- 		
 				</div>
+				<span class="nav-text">#Zypher</span>
 			</nav>
 		)
 	};
